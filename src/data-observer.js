1beb472df25088104cee3e67029761c300cd1483
Guller Yuri <yuri.guller@hp.com>
2016-04-04 01:13:48 +0300

    initial commit

---
+++ src/data-observer.js
@@ -0,0 +1,118 @@
+﻿(function (scope) {
+	'use strict';
+
+	var pathsMap = new WeakMap();
+
+	function iterate(graph, result, path) {
+		var tmp, pVal;
+		if (Array.isArray(graph)) {
+			tmp = path + '[';
+			graph.forEach(function (itm, idx) {
+				if (itm && typeof itm === 'object') {
+					iterate(itm, result, [tmp, idx, ']'].join(''));
+				} else {
+					result[[tmp, idx, ']'].join('')] = itm;
+				}
+			});
+		} else {
+			tmp = (path ? path + '.' : '');
+			Reflect.ownKeys(graph).forEach(function (pKey) {
+				pVal = graph[pKey];
+				if (pVal && typeof pVal === 'object') {
+					iterate(pVal, result, tmp + pKey);
+				} else {
+					result[tmp + pKey] = pVal;
+				}
+			});
+		}
+	}
+
+	function flattenObject(graph) {
+		var result = {};
+		if (typeof graph !== 'object') { throw new Error('illegal graph argument, object expected'); }
+		if (graph) { iterate(graph, result, ''); }
+		return result;
+	}
+
+	function calculateGraphChange(oldGraph, newGraph) {
+		var oldGraphFlat,
+			newGraphFlat,
+			result;
+
+		oldGraphFlat = flattenObject(oldGraph);
+		newGraphFlat = flattenObject(newGraph);
+		//	generate list of changes
+
+		return result;
+	}
+
+	function DataObserver() {
+
+		function observe(target, callback) {
+			if (!target || typeof target !== 'object') {
+				throw new Error('target MUST be a non-empty object');
+			}
+			if (typeof callback !== 'function') {
+				throw new Error('callback parameter MUST be a function');
+			}
+
+			return Proxy.revocable(target, {
+				set: function proxiedSet(target, key, value) {
+					var oldValue = target[key],
+						result,
+						changes = [],
+						path;
+
+					result = Reflect.set(target, key, value);
+					path = pathsMap.has(target) ? [pathsMap.get(target), key].join('.') : key;
+					if (result) {
+						if (typeof oldValue === 'object' && oldValue) {
+							//	remove old proxy
+						}
+						if (typeof value === 'object' && value) {
+							pathsMap.set(value, path);
+							Reflect.set(target, key, observe(value, callback));
+						}
+						//	instead of simple callback below, an array of changes should be delivered
+						callback(path, value, oldValue);
+					}
+					return result;
+				},
+				deleteProperty: function proxiedDelete(target, key) {
+					var oldValue = target[key],
+						result,
+						changes = [],
+						path;
+
+					result = Reflect.deleteProperty(target, key);
+					if (result) {
+						if (typeof oldValue === 'object' && oldValue) {
+							pathsMap.get(oldValue).revoke();
+							pathsMap.delete(oldValue);
+							//	calc flat paths and build an array of changes
+						} else {
+							path = pathsMap.has(target) ? [pathsMap.get(target), key].join('.') : key;
+							changes.push({
+								path: path,
+								oldValue: oldValue
+							});
+						}
+						changes.forEach(callback);
+					}
+					return result;
+				}
+			}).proxy;
+		}
+
+		Reflect.defineProperty(this, 'getObserved', { value: observe });
+		Reflect.defineProperty(this, 'details', {
+			value: {
+				description: 'Proxy driven data observer implementation'
+			}
+		});
+	}
+
+	Reflect.defineProperty(scope, 'DataObserver', { value: DataObserver });
+	Reflect.defineProperty(scope, 'flatten', { value: flattenObject });
+})(this);
+
---
+++ tests/testDataObserver-Proxy.js
@@ -0,0 +1,70 @@
+﻿(function () {
+	'use strict';
+
+	var suite = window.Utils.JustTest.createSuite({ name: 'Testing DataObserver' }), observer = new DataObserver();
+
+	if (observer.details.description.indexOf('Proxy') < 0) {
+		console.info('data observer not of a Proxy implementation - skipping non relevant tests');
+		return;
+	}
+
+	suite.addTest({ name: 'test A - plain object operations' }, function (pass, fail) {
+		var o = {
+			name: 'name',
+			age: 7,
+			address: null
+		}, po, events = [], tmpAddress = { street: 'some' };
+
+		po = observer.getObserved(o, function (path, value, oldValue) {
+			events.push({
+				p: path,
+				v: value,
+				o: oldValue
+			});
+		});
+
+		po.name = 'new name';
+		po.age = 9;
+		po.address = tmpAddress;
+		po.address = null;
+		po.sex = 'male';
+		delete po.sex;
+
+		if (events.length !== 6) fail('expected to have 6 data change events but counted ' + events.length);
+		if (events[0].p !== 'name' || events[0].o !== 'name' || events[0].v !== 'new name') fail('event 0 did not fire as expected');
+		if (events[1].p !== 'age' || events[1].o !== 7 || events[1].v !== 9) fail('event 1 did not fire as expected');
+		if (events[2].p !== 'address' || events[2].o !== null || events[2].v !== tmpAddress) fail('event 2 did not fire as expected');
+		if (events[3].p !== 'address' || events[3].v !== null) fail('event 3 did not fire as expected');
+		if (events[4].p !== 'sex' || typeof events[4].o !== 'undefined' || events[4].v !== 'male') fail('event 4 did not fire as expected');
+		if (events[5].p !== 'sex' || events[5].o !== 'male' || typeof events[5].v !== 'undefined') fail('event 5 did not fire as expected');
+
+		pass();
+	});
+
+	suite.addTest({ name: 'test A - sub tree object operations' }, function (pass, fail) {
+		var o = {
+			name: 'name',
+			age: 7,
+			address: null
+		}, po, events = [], newAddress = {};
+
+		po = observer.getObserved(o, function (path, value, oldValue) {
+			events.push({
+				p: path,
+				v: value,
+				o: oldValue
+			});
+		});
+
+		po.address = newAddress;
+		po.address.street = 'street';
+
+		if (events.length !== 2) fail('expected to have 2 data change events but counted ' + events.length);
+		if (events[0].p !== 'address' || events[0].o !== null || events[0].v !== newAddress) fail('event 0 did not fire as expected');
+		if (events[1].p !== 'address.street' || typeof events[1].o !== 'undefined' || events[1].v !== 'street') fail('event 1 did not fire as expected');
+
+		pass();
+	});
+
+	suite.run();
+})();
