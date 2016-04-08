79f0f44deac89d5065860b09afe3392dd8fe8076
Yuri Guller <gullerya@gmail.com>
2016-04-08 13:21:28 +0300

    wip

--- src/object-observer.js
+++ src/object-observer.js
@@ -1,8 +1,104 @@
 ﻿(function (scope) {
 	'use strict';
 
-	var pathsMap = new WeakMap();
+	var observables = new WeakMap(),
+		pathsMap = new WeakMap(),
+		details;
 
+	//	PUBLIC APIs
+	//
+	function createObservable(target) {
+		var result,
+			rootTarget = arguments[1] || target;
+
+		result = Proxy.revocable(target, {
+			set: function proxiedSet(target, key, value) {
+				var oldValue = target[key],
+					result,
+					changes = [],
+					path;
+
+				result = Reflect.set(target, key, value);
+				path = pathsMap.has(target) ? [pathsMap.get(target), key].join('.') : key;
+				if (result) {
+					if (typeof oldValue === 'object' && oldValue) {
+						//	remove old proxy
+					}
+					if (typeof value === 'object' && value) {
+						pathsMap.set(value, path);
+						Reflect.set(target, key, createObservable(value, rootTarget));
+					}
+					observables.get(rootTarget).forEach(function (callback) {
+						try {
+							callback(changes);
+						} catch (e) {
+							console.error(e);
+						}
+					});
+				}
+				return result;
+			},
+			deleteProperty: function proxiedDelete(target, key) {
+				var oldValue = target[key],
+					result,
+					changes = [],
+					path;
+
+				result = Reflect.deleteProperty(target, key);
+				if (result) {
+					if (typeof oldValue === 'object' && oldValue) {
+						pathsMap.get(oldValue).revoke();
+						pathsMap.delete(oldValue);
+						//	calc flat paths and build an array of changes
+					} else {
+						path = pathsMap.has(target) ? [pathsMap.get(target), key].join('.') : key;
+						changes.push({
+							path: path,
+							oldValue: oldValue
+						});
+					}
+					observables.get(rootTarget).forEach(function (callback) {
+						try {
+							callback(changes);
+						} catch (e) {
+							console.error(e);
+						}
+					});
+				}
+				return result;
+			}
+		}).proxy;
+
+		return result;
+	}
+
+	function observe(observable, callback) {
+		if (!observables.has(observable)) {
+			throw new Error(observable + ' is not a known observable');
+		}
+		if (typeof callback !== 'function') {
+			throw new Error('callback parameter MUST be a function');
+		}
+
+		observables.get(observable).push(callback);
+	}
+
+	function unobserve(observable) {
+		if (!observables.has(observable)) {
+			throw new Error(observable + ' is not a known observable');
+		}
+
+		if (arguments.length > 1) {
+			for (let i = 1; i < arguments.length; i++) {
+				//	if callbacks list contains argument[i] - remove it
+			}
+		} else {
+			observables.get(observable) = [];
+		}
+	}
+
+	//	INTERNALS
+	//
 	function iterate(graph, result, path) {
 		var tmp, pVal;
 		if (Array.isArray(graph)) {
@@ -46,73 +142,22 @@
 		return result;
 	}
 
-	function DataObserver() {
-
-		function observe(target, callback) {
-			if (!target || typeof target !== 'object') {
-				throw new Error('target MUST be a non-empty object');
-			}
-			if (typeof callback !== 'function') {
-				throw new Error('callback parameter MUST be a function');
-			}
-
-			return Proxy.revocable(target, {
-				set: function proxiedSet(target, key, value) {
-					var oldValue = target[key],
-						result,
-						changes = [],
-						path;
-
-					result = Reflect.set(target, key, value);
-					path = pathsMap.has(target) ? [pathsMap.get(target), key].join('.') : key;
-					if (result) {
-						if (typeof oldValue === 'object' && oldValue) {
-							//	remove old proxy
-						}
-						if (typeof value === 'object' && value) {
-							pathsMap.set(value, path);
-							Reflect.set(target, key, observe(value, callback));
-						}
-						//	instead of simple callback below, an array of changes should be delivered
-						callback(path, value, oldValue);
-					}
-					return result;
-				},
-				deleteProperty: function proxiedDelete(target, key) {
-					var oldValue = target[key],
-						result,
-						changes = [],
-						path;
-
-					result = Reflect.deleteProperty(target, key);
-					if (result) {
-						if (typeof oldValue === 'object' && oldValue) {
-							pathsMap.get(oldValue).revoke();
-							pathsMap.delete(oldValue);
-							//	calc flat paths and build an array of changes
-						} else {
-							path = pathsMap.has(target) ? [pathsMap.get(target), key].join('.') : key;
-							changes.push({
-								path: path,
-								oldValue: oldValue
-							});
-						}
-						changes.forEach(callback);
-					}
-					return result;
-				}
-			}).proxy;
+	Reflect.defineProperty(this, 'details', {
+		value: {
+			description: 'Proxy driven data observer implementation'
 		}
-
-		Reflect.defineProperty(this, 'getObserved', { value: observe });
-		Reflect.defineProperty(this, 'details', {
-			value: {
-				description: 'Proxy driven data observer implementation'
+	});
+	Reflect.defineProperty(scope, 'createObservable', {
+		value: function (target) {
+			if (!target || typeof target !== 'object') {
+				throw new Error('observable may be created from non null object only');
 			}
-		});
-	}
-
-	Reflect.defineProperty(scope, 'DataObserver', { value: DataObserver });
+			let result = createObservable(target);
+			observables.set(target, []);
+			return result;
+		}
+	});
+	Reflect.defineProperty(scope, 'observe', { value: observe });
+	Reflect.defineProperty(scope, 'unobserve', { value: unobserve });
 	Reflect.defineProperty(scope, 'flatten', { value: flattenObject });
 })(this);
-
