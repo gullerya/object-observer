c2896712241824b39c831872f8dc4f92c9a5cafe
Yuri Guller <gullerya@gmail.com>
2018-09-13 22:15:59 +0300

    tech: added toplevet option for better minification/compression

--- build/tools/build-dist.js
+++ build/tools/build-dist.js
@@ -13,16 +13,19 @@ fsExtra.copySync('./src', './dist');
 process.stdout.write('\t\x1B[32mOK\x1B[0m' + os.EOL);
 
 process.stdout.write('minifying...');
+let options = {
+	toplevel: true
+};
 fs.writeFileSync(
 	'./dist/module/object-observer.min.js',
-	uglifyES.minify(fs.readFileSync('./dist/module/object-observer.js', {encoding: 'utf8'})).code
+	uglifyES.minify(fs.readFileSync('./dist/module/object-observer.js', {encoding: 'utf8'}), options).code
 );
 fs.writeFileSync(
 	'./dist/node-module/object-observer.min.js',
-	uglifyES.minify(fs.readFileSync('./dist/node-module/object-observer.js', {encoding: 'utf8'})).code
+	uglifyES.minify(fs.readFileSync('./dist/node-module/object-observer.js', {encoding: 'utf8'}), options).code
 );
 fs.writeFileSync(
 	'./dist/object-observer.min.js',
-	uglifyES.minify(fs.readFileSync('./dist/object-observer.js', {encoding: 'utf8'})).code
+	uglifyES.minify(fs.readFileSync('./dist/object-observer.js', {encoding: 'utf8'}), options).code
 );
 process.stdout.write('\t\t\t\t\x1B[32mOK\x1B[0m' + os.EOL);
