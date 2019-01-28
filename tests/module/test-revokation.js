08969249230a8d57033e1b3828ef88cfc1e02f74
gullery <gullerya@gmail.com>
2019-01-28 09:48:57 +0200

    issue #18: adjusting other tests expectations to the correct ones

--- tests/module/test-revokation.js
+++ tests/module/test-revokation.js
@@ -2,7 +2,7 @@
 
 let suite = Utils.JustTest.createSuite({name: 'Testing revokation of removed/replaced objects'});
 
-suite.addTest({name: 'test revokation of replaced objects'}, function (pass, fail) {
+suite.addTest({name: 'test revokation of replaced objects - simple set'}, function (pass, fail) {
 	let g = {
 		a: {
 			b: {
@@ -22,6 +22,30 @@ suite.addTest({name: 'test revokation of replaced objects'}, function (pass, fai
 
 	og.a = og.a.b;
 	if (og.a.prop !== 'text') fail('expected the new value on the observed graph to be accessible');
+
+	pass();
+});
+
+suite.addTest({name: 'test revokation of replaced objects - splice in array'}, function (pass, fail) {
+	let g = [
+		{
+			child: {
+				prop: 'text'
+			},
+			prop: 'text'
+		}
+	];
+	let eventsCollector = [];
+	let og = Observable.from(g);
+	og.observe(changes => {
+		eventsCollector = eventsCollector.concat(changes);
+		if (changes.length !== 1 || changes[0].type !== 'update') fail('expected to track one update change');
+		if (changes[0].oldValue.prop !== 'text') fail('expected the old value to still be readable');
+		if (changes[0].value.prop !== 'text') fail('expected the new value to be readable');
+	});
+
+	og.splice(0, 1, og[0].child);
+	if (og[0].prop !== 'text') fail('expected the new value on the observed graph to be accessible');
 
 	pass();
 });
