0c73e5898d4f33b4cbbd1dadcff45a9f8a0531a4
gullerya <gullerya@gmail.com>
2019-11-07 15:57:21 +0200

    issue #24: added some tests

--- tests/module/test-object-observer-native-objects-to-skip.js
+++ tests/module/test-object-observer-native-objects-to-skip.js
@@ -3,7 +3,7 @@ import { Observable } from '../../dist/object-observer.js';
 
 const suite = createSuite({ name: 'Testing Observable - non-observables' });
 
-suite.runTest({ name: 'creating observable from non-observable should throw an error' }, () => {
+suite.runTest({ name: 'creating observable from non-observable should throw an error' }, test => {
 	let objectsToTest = [
 		new Date(),
 		new Blob(),
@@ -14,17 +14,19 @@ suite.runTest({ name: 'creating observable from non-observable should throw an e
 		new Function(),
 		new RegExp(),
 		new Promise(function () {
-		})],
-		o;
+		})];
 
 	objectsToTest.forEach(function (one) {
 		try {
-			o = Observable.from(one);
+			const o = Observable.from(one);
 			throw new Error('should not get to this point');
 		} catch (e) {
 			//	do nothing here
 		}
 	});
+
+	const o = Observable.from(objectsToTest);
+	test.assertTrue(objectsToTest.every(one => o.some(oo => oo === one)));
 });
 
 suite.runTest({ name: 'non-observable in an object subgraph should stay unchanged' }, () => {
@@ -45,7 +47,7 @@ suite.runTest({ name: 'non-observable in an object subgraph should stay unchange
 	po = Observable.from(o);
 	Object.keys(o).forEach(function (key) {
 		if (key === 'object') {
-			if (o[key] === po[key]) throw new Error('proxification on regular object throw new Errored - test is malfunctioning');
+			if (o[key] === po[key]) throw new Error('proxification on regular object throw new Error - test is malfunctioning');
 		} else {
 			if (o[key] !== po[key]) throw new Error('expected non-observable key ' + key + ' to remain the same in the Observable');
 		}
@@ -70,7 +72,7 @@ suite.runTest({ name: 'non-observable in an array subgraph should stay unchanged
 	o = Observable.from(a);
 	a.forEach(function (elem, index) {
 		if (index === 0) {
-			if (a[index] === o[index]) throw new Error('proxification on regular object throw new Errored - test is malfunctioning');
+			if (a[index] === o[index]) throw new Error('proxification on regular object throw new Error - test is malfunctioning');
 		} else {
 			if (a[index] !== o[index]) throw new Error('expected non-observable index ' + index + ' to remain the same in the Observable');
 		}
@@ -102,6 +104,21 @@ suite.runTest({ name: 'non-observable should be handled correctly when replaced'
 	test.assertTrue(oo.error instanceof Error);
 	test.assertTrue('name' in oo.error);
 	test.assertTrue('message' in oo.error);
+	test.assertTrue('stack' in oo.error);
+});
+
+suite.runTest({ name: 'non-observable deviation should be handled correctly when replaced' }, test => {
+	let o = {
+		error: new SyntaxError('syntax error message')
+	}, oo = Observable.from(o);
+
+	test.assertEqual(oo.error, o.error);
+
+	oo.error = new TypeError('type error message');
+
+	test.assertTrue(oo.error instanceof TypeError);
+	test.assertTrue('name' in oo.error);
+	test.assertEqual(oo.error.message, 'type error message');
 	test.assertTrue('stack' in oo.error);
 });
 
