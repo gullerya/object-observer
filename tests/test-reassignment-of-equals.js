ebbf15d75f67d601cc63494894b8ec0d4b3f80d2
Yuri Guller <gullerya@gmail.com>
2020-08-23 08:23:03 +0300

    updated dependencies of test framework, yet to fix tests

--- tests/test-reassignment-of-equals.js
+++ tests/test-reassignment-of-equals.js
@@ -1,4 +1,4 @@
-﻿import { getSuite } from '../../node_modules/just-test/dist/just-test.min.js';
+﻿import { getSuite } from '../../node_modules/just-test/dist/just-test.js';
 import { Observable } from '../../dist/object-observer.js';
 
 const suite = getSuite({ name: 'Testing reassignment of equal values' });
