035d31f3255340158ab12021f57e5fd834539b8d
Yuri Guller <gullerya@gmail.com>
2020-09-03 22:58:42 +0300

    switching paradigm to test sources and not dist

--- tests/test-object-observer-arrays-typed.js
+++ tests/test-object-observer-arrays-typed.js
@@ -1,5 +1,5 @@
 import { getSuite } from '../../node_modules/just-test/dist/just-test.js';
-import { Observable } from '../../dist/object-observer.js';
+import { Observable } from '../../src/object-observer.js';
 
 const suite = getSuite({ name: 'Testing ObjectObserver - typed arrays' });
 
