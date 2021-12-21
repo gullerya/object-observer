f3929fe1c4d3a77ef2e4fbcfedb673b779b96e62
gullerya <gullerya@gmail.com>
2021-12-21 07:54:19 +0200

    fixing the ts

--- src/object-observer.d.ts
+++ src/object-observer.d.ts
@@ -7,21 +7,24 @@ export enum ChangeType {
 }
 
 /**
- * Observable allows to observe any (deep) changes on it's underlying object graph
+ * `Observable` allows to observe any (deep) changes on it's underlying object graph
+ * - created by `from` static method, via cloning the target and enhancing it with own methods
+ * - important: the type `T` is not preserved, beside the shape
  */
 export abstract class Observable {
 
 	/**
 	 * create Observable from the target
-	 * - target is cloned, remaining unchange on itself
+	 * - target is cloned, remaining unchanged in itself
+	 * - important: the type `T` is NOT preserved, beside it's shape
 	 */
-	static from: (target: object, options?: ObservableOptions) => Observable;
+	static from<T>(target: T, options?: ObservableOptions): Observable & T;
 
-	static isObservable: (input: object) => boolean;
+	static isObservable(input: unknown): boolean;
 
-	observe: (observer: Observer, options?: ObserverOptions) => void;
+	abstract observe(observer: Observer, options?: ObserverOptions): void;
 
-	unobserve: (...observer: Observer[]) => void;
+	abstract unobserve<T>(...observer: Observer[]): T;
 }
 
 export interface ObservableOptions {
@@ -47,15 +50,32 @@ export interface Change {
 }
 
 /**
- * ObjectObserver provides observation functionality in a WebAPI-like flavor
+ * `ObjectObserver` provides observation functionality in a WebAPI-like flavor
+ * - `observer` created first, with the provided observer function
+ * - `observer` may then be used to observe different targets
  */
 export class ObjectObserver {
 
-	constructor(observer: Observer);
+	/**
+	 * sets up observer function and options
+	 * @param observer observation logic (function)
+	 * @param options  `ObservableOptions` will be applied to any `Observable` down the road
+	 */
+	constructor(observer: Observer, options?: ObservableOptions);
 
-	observe(target: object, options: ObserverOptions): Observable;
+	/**
+	 * created `Observable` from the target and starts observation
+	 * - important: the type `T` is NOT preserved, except the shape
+	 * @param target  target to be observed, turned into `Observable` via cloning during the process
+	 * @param options `ObserverOptions` options
+	 */
+	observe<T>(target: T, options: ObserverOptions): Observable & T;
 
-	unobserve(target: object): void;
+	/**
+	 * un-observes the `Observable`, if any, returning the original undelying plain object
+	 * @param target target to be un-observed
+	 */
+	unobserve<T>(target: (Observable & T) | unknown): T;
 
 	disconnect(): void;
 }
