a9d02bf6cebadc888be7b66b7721d799c594a6b8
Yuri Guller <gullerya@gmail.com>
2022-06-30 11:16:49 +0300

    adding more tests

--- tests/object-observer-subgraphs.js
+++ tests/object-observer-subgraphs.js
@@ -1,9 +1,10 @@
-import { getSuite } from '../../node_modules/just-test/dist/just-test.js';
-import { Observable } from '../../src/object-observer.js';
+import { assert } from 'chai';
+import { getSuite } from 'just-test/suite';
+import { Observable } from '../src/object-observer.js';
 
-const suite = getSuite({ name: 'Testing Observable - subgraphs' });
+const suite = getSuite('Testing Observable - subgraphs');
 
-suite.runTest({ name: 'inner object from observable should fire events as usual' }, () => {
+suite.test('inner object from observable should fire events as usual', () => {
 	const
 		oo = Observable.from({ inner: { prop: 'more' } }),
 		iop = oo.inner,
@@ -16,12 +17,12 @@ suite.runTest({ name: 'inner object from observable should fire events as usual'
 	iop.prop = 'else';
 	iop.new = 'prop';
 
-	if (events.length !== 2) throw new Error('expected 2 callbacks on inner observer graph (subgraph), but found ' + events.length);
-	if (events[0].type !== 'update' || events[0].path.join('.') !== 'inner.prop' || events[0].oldValue !== 'more' || events[0].value !== 'else' || events[0].object !== iop) throw new Error('event 0 did not fire as expected');
-	if (events[1].type !== 'insert' || events[1].path.join('.') !== 'inner.new' || typeof events[1].oldValue !== 'undefined' || events[1].value !== 'prop' || events[1].object !== iop) throw new Error('event 1 did not fire as expected');
+	assert.strictEqual(events.length, 2);
+	assert.deepStrictEqual(events[0], { type: 'update', path: ['inner', 'prop'], value: 'else', oldValue: 'more', object: iop });
+	assert.deepStrictEqual(events[1], { type: 'insert', path: ['inner', 'new'], value: 'prop', oldValue: undefined, object: iop });
 });
 
-suite.runTest({ name: 'removal (detaching) of inner object from observable should detach its events' }, test => {
+suite.test('removal (detaching) of inner object from observable should detach its events', () => {
 	const
 		oo = Observable.from({ inner: { prop: 'more' } }),
 		iop = oo.inner,
@@ -32,39 +33,30 @@ suite.runTest({ name: 'removal (detaching) of inner object from observable shoul
 
 	Observable.observe(oo, observer);
 	iop.prop = 'text';
-	test.assertEqual(1, cntr);
+	assert.strictEqual(cntr, 1);
 
 	oo.inner = null;
 	cntr = 0;
 	iop.prop = 'again';
-	test.assertEqual(0, cntr);
+	assert.strictEqual(cntr, 0);
 });
 
-suite.runTest({ name: 'replacement of inner object from observable should return non-proxified original object' }, () => {
+suite.test('replacement of inner object from observable should return non-proxified original object', () => {
 	const oo = Observable.from({ inner: { prop: 'more', nested: { text: 'text' } } });
 	let events = [];
 	Observable.observe(oo, changes => Array.prototype.push.apply(events, changes));
 
 	oo.inner.prop = 'some';
-	if (events.length !== 1 || events[0].type !== 'update' || events[0].path.length !== 2 || events[0].object !== oo.inner) {
-		throw new Error('expected to have correct update event');
-	}
+	assert.strictEqual(events.length, 1);
+	assert.deepStrictEqual(events[0], { type: 'update', path: ['inner', 'prop'], value: 'some', oldValue: 'more', object: oo.inner });
 	events = [];
 
 	oo.inner = { p: 'back' };
-	if (events.length !== 1 || events[0].type !== 'update' || events[0].path.length !== 1 || events[0].object !== oo) {
-		throw new Error('expected to have correct update event - metadata');
-	} else {
-		if (!events[0].oldValue || events[0].oldValue.prop !== 'some') {
-			throw new Error('expected to have correct update event - correct oldValue present');
-		}
-		if (events[0].oldValue.nested.text !== 'text') {
-			throw new Error('expected to have correct update event - correct oldValue nested object present');
-		}
-	}
+	assert.strictEqual(events.length, 1);
+	assert.deepStrictEqual(events[0], { type: 'update', path: ['inner'], value: { p: 'back' }, oldValue: { prop: 'some', nested: { text: 'text' } }, object: oo });
 });
 
-suite.runTest({ name: 'Object.assign on observable should raise an event/s of update with non-proxified original object' }, () => {
+suite.test('Object.assign on observable should raise an event/s of update with non-proxified original object', () => {
 	const
 		observable = Observable.from({ b: { b1: 'x', b2: 'y' } }),
 		newData = { b: { b1: 'z' } },
@@ -73,11 +65,6 @@ suite.runTest({ name: 'Object.assign on observable should raise an event/s of up
 
 	Object.assign(observable, newData);
 
-	if (events.length !== 1 || events[0].type !== 'update' || events[0].path.join('.') !== 'b' || events[0].object !== observable) {
-		throw new Error('UPDATE event metadata NOT as expected');
-	} else if (!events[0].value || events[0].value.b1 !== 'z') {
-		throw new Error('UPDATE event (new) value NOT as expected');
-	} else if (!events[0].oldValue || events[0].oldValue.b1 !== 'x' || events[0].oldValue.b2 !== 'y') {
-		throw new Error('UPDATE event oldValue NOT as expected');
-	}
+	assert.strictEqual(events.length, 1);
+	assert.deepStrictEqual(events[0], { type: 'update', path: ['b'], value: { b1: 'z' }, oldValue: { b1: 'x', b2: 'y' }, object: observable });
 });
