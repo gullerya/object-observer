b029de9f585e18d923833a5caa813add08c758ec
Yuri Guller <gullerya@gmail.com>
2022-09-03 17:14:53 +0300

    adding perf tests

--- tests/object-observer-performance-async.js
+++ tests/object-observer-performance-async.js
@@ -1,6 +1,6 @@
-import { getSuite } from '../../node_modules/just-test/dist/just-test.js';
+import { getSuite } from 'just-test/suite';
 
-const suite = getSuite({ name: 'Testing Observable load - async' });
+const suite = getSuite('Testing Observable load - async');
 
 const TOLERANCE_MULTIPLIER = 5;
 
@@ -8,12 +8,10 @@ const
 	CREATE_ITERATIONS = 100000,
 	MUTATE_ITERATIONS = 1000000;
 
-suite.runTest({
-	name: `creating ${CREATE_ITERATIONS} observables, ${MUTATE_ITERATIONS} deep (x3) mutations`,
-	sync: true,
-	timeout: 15000
+suite.test(`creating ${CREATE_ITERATIONS} observables, ${MUTATE_ITERATIONS} deep (x3) mutations`, {
+	ttl: 15000
 }, async () => {
-	await executeInWorker('/tests/workers/perf-async-test-a.js', {
+	await executeInWorker('perf-async-test-a.js', {
 		TOLERANCE_MULTIPLIER: TOLERANCE_MULTIPLIER,
 		CREATE_ITERATIONS: CREATE_ITERATIONS,
 		MUTATE_ITERATIONS: MUTATE_ITERATIONS,
@@ -26,12 +24,10 @@ suite.runTest({
 
 const ARRAY_ITERATIONS = 100000;
 
-suite.runTest({
-	name: `push ${ARRAY_ITERATIONS} observables to an array, mutate them and pop them back`,
-	sync: true,
-	timeout: 15000
+suite.test(`push ${ARRAY_ITERATIONS} observables to an array, mutate them and pop them back`, {
+	ttl: 15000
 }, async () => {
-	return executeInWorker('/tests/workers/perf-async-test-b.js', {
+	await executeInWorker('perf-async-test-b.js', {
 		TOLERANCE_MULTIPLIER: TOLERANCE_MULTIPLIER,
 		ARRAY_ITERATIONS: ARRAY_ITERATIONS,
 		ARRAY_PUSH_TRSHLD: 0.005,
@@ -42,21 +38,26 @@ suite.runTest({
 
 async function executeInWorker(testUrl, testParams) {
 	// return (await import(testUrl)).default(testParams);
+
+	let CrossPlatformWorker = globalThis.Worker;
+	if (!CrossPlatformWorker) {
+		CrossPlatformWorker = (await import('node:worker_threads')).default.Worker;
+	}
+
 	return new Promise((resolve, reject) => {
-		const w = new Worker('./workers/perf-worker.js');
-		w.onmessage = message => {
+		const w = new CrossPlatformWorker('./tests/workers/perf-worker.js');
+		w.on('message', message => {
 			w.terminate();
-			if (message.data && message.data.error) {
-				console.error(`failed in worker; name: '${message.data.error.name}', message: '${message.data.error.message}'`);
-				reject(new Error(message.data.error.message));
+			if (message?.error) {
+				reject(new Error(message.error.message));
 			} else {
 				resolve();
 			}
-		};
-		w.onerror = error => {
+		});
+		w.on('error', error => {
 			w.terminate();
 			reject(error);
-		};
+		});
 		w.postMessage({ testUrl: testUrl, testParams: testParams });
 	});
 }
